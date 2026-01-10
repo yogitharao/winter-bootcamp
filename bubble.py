0e6b094b8c6fb223ac271307fe432236dd52ae92
yogitharao <yogitharao.dagani.s.134@kalvium.community>
2026-01-10 16:04:23 +0530

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,15 @@
+class Solution:
+    def bubbleSort(self, nums):
+        n = len(nums)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+            if not swapped:
+                break
+        return nums
+nums=list(map(int,input().split()))
+sol=Solution()
+print(sol.bubbleSort(nums))
